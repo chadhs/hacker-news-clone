057ede0b49f17715fd980272bd0523dc78dbc05e
Chad Stovern <chad@stovern.me>
2020-05-28 14:52:05 -0500

    created a comment view for stories/posts
    need to pull in the post's metadata yet

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,8 +1,44 @@
 import React from 'react';
 import { ThemeContext } from '../contexts/theme';
 import queryString from 'query-string';
+import { fetchItemById, fetchStories } from '../utils/api';
+import { formatDateTimeMetadata } from '../utils/time';
 
 export const Comments = ({ location }) => {
   const { id: postId } = queryString.parse(location.search);
-  return <div>Comments Story {postId}</div>;
+
+  const [comments, setComments] = React.useState([]);
+  const theme = React.useContext(ThemeContext);
+
+  React.useEffect(() => {
+    const getAndSetComments = async () => {
+      const { kids: commentIds } = await fetchItemById(postId);
+      if (commentIds?.length > 0) {
+        setComments(await fetchStories(commentIds));
+      }
+    };
+
+    getAndSetComments();
+  }, [location, postId]);
+
+  return (
+    <div>
+      <div className={`story-title story-link-${theme}`}>
+        Story Title and Link {postId}
+      </div>
+      <div className="story-metadata">Story Metadata</div>
+      {comments.map((comment) => (
+        <div key={comment.id} className={`Comment-${theme}`}>
+          <div className="story-metadata">
+            by{' '}
+            <a href="/" className={`${theme}`}>
+              {comment.by}
+            </a>{' '}
+            on {formatDateTimeMetadata(comment.time)}
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: comment.text }} />
+        </div>
+      ))}
+    </div>
+  );
 };
